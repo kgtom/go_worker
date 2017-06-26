7bfc00b7743cb37bd2404a18258e7dc30b2fad0c
tom <leiweizhen@zailushang.cn>
2017-06-26 10:43:46 +0800

    fix deadlock by timer

--- worker.go
+++ worker.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"sync"
+	"time"
 )
 
 type safepending struct {
@@ -25,6 +26,7 @@ func (s *safepending) Dec() {
 type job struct {
 	// something
 	Id int
+	//result chan ResultType //可能需要返回处理结果给其他channel
 }
 
 type jobPair struct {
@@ -37,6 +39,8 @@ type worker struct {
 	pending  *safepending
 	jobadd   chan *jobPair
 	jobdel   chan string
+	index    int
+	done     chan struct{}
 }
 
 // 并不是真正的map insert操作，仅发消息给另外一个线程
@@ -55,10 +59,22 @@ func (w *worker) RemoveJob(user string) {
 }
 func main() {
 	fmt.Println("main start.....")
-	worker := new(worker)
-	job := &job{Id: 1}
+	// worker := new(worker)
+
+	//job := &job{Id: 1}
+	// worker.jobadd = make(chan *jobPair)
+	// worker.jobdel = make(chan string)
+	worker := &worker{
+		jobqueue: make(map[string]*job),
+		pending:  new(safepending),
+		jobadd:   make(chan *jobPair),
+		jobdel:   make(chan string),
+		index:    5,
+		done:     make(chan struct{}),
+	}
 	var wg sync.WaitGroup
 	worker.Run(&wg)
+	job := &job{Id: 1}
 	worker.PushJob("tom", job)
 	wg.Wait()
 }
@@ -67,6 +83,8 @@ func (w *worker) Run(wg *sync.WaitGroup) {
 
 	go func() {
 		defer wg.Done()
+		ticker := time.NewTicker(time.Second * 3)
+		defer ticker.Stop()
 		for {
 			select {
 			case jobpair := <-w.jobadd:
@@ -74,8 +92,32 @@ func (w *worker) Run(wg *sync.WaitGroup) {
 				fmt.Println("case insert....", jobpair.key)
 			case delkey := <-w.jobdel:
 				w.deleteJob(delkey)
+			case <-ticker.C:
+				fmt.Println("timeout")
+				return
+			default:
+				//fmt.Println("end")
 
 			}
+			//注意：
+			//当case <- jobpair/delkey 和 case <- ticker.C 同时成立时，Select会//随机公平地选出一个执行，有可能选择到前者，导致超时随机行失败
+			//解决方案：将【超时】和【收包】放在各自单独的select里面，【超时】一定可以执行到
+			// 	 select {
+			// case jobpair := <-w.jobadd:
+			// 	w.insertJob(jobpair.key, jobpair.value)
+			// 	fmt.Println("case insert....", jobpair.key) // if ch not empty, time.After will nerver exec
+			//         fmt.Println("sleep one seconds ...")
+			//         time.Sleep(1 * time.Second)
+			//         fmt.Println("sleep one seconds end...")
+			//     default: // forbid block
+			// }
+			// select {
+			//     case <- ticker.C:
+			//         fmt.Println("timeout")
+			//         return
+			//     default: // forbid block
+			// }
+			//参考：http://studygolang.com/articles/5224
 		}
 	}()
 
